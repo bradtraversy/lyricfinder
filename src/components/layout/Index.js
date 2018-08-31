b02bb888f079c89fb7a8d731a8d0e047a4e21d77
Brad Traversy <traversymedia@gmail.com>
2018-08-31 16:57:40 -0400

    Search component & request

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Tracks from '../tracks/Tracks';
+import Search from '../tracks/Search';
 
 const Index = () => {
   return (
     <React.Fragment>
+      <Search />
       <Tracks />
     </React.Fragment>
   );
---
+++ src/components/tracks/Search.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Consumer } from '../../context';
+
+class Search extends Component {
+  state = {
+    trackTitle: ''
+  };
+
+  findTrack = e => {
+    e.preventDefault();
+
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${
+          this.state.trackTitle
+        }&page_size=10&page=1&s_track_rating=desc&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      )
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          return (
+            <div className="card card-body mb-4 p-4">
+              <h1 className="display-4 text-center">
+                <i className="fas fa-music" /> Search For A Song
+              </h1>
+              <p className="lead text-center">Get the lyrics for any song</p>
+              <form onSubmit={this.findTrack}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Song title..."
+                    name="trackTitle"
+                    value={this.state.trackTitle}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <button
+                  className="btn btn-primary btn-lg btn-block mb-5"
+                  type="submit"
+                >
+                  Get Track Lyrics
+                </button>
+              </form>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Search;
