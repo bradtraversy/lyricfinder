8a556e6fe6639a8c48860730dbbadf7c96a61c7f
Brad Traversy <traversymedia@gmail.com>
2018-08-31 16:42:36 -0400

    Initial cra boilerplate cleanup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
