47e6b65193c8225bb52d2938617f462d72c31842
Brad Traversy <traversymedia@gmail.com>
2018-08-31 16:44:03 -0400

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 h1 mx-auto">LyricFinder</span>
+    </nav>
+  );
+};
+
+export default Navbar;
